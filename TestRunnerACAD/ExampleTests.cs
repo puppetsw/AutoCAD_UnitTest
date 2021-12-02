0e3ab96cd3805fd43482fa8ee4580c9ad0ed7a18
Scott Whitney <scottwhits@gmail.com>
2021-12-03 09:21:58 +1030

    Initial Commit

---
+++ TestRunnerACAD/ExampleTests.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading;
+using NUnit.Framework;
+
+namespace TestRunnerACAD
+{
+    [TestFixture, Apartment(ApartmentState.STA)]
+    public class ExampleTests : TestBase
+    {
+        [Test]
+        public void Test_Pass()
+        {
+            Assert.Pass("This test should always passes.");
+        }
+    }
+}
---
+++ TestRunnerACAD/TestRunner.cs
@@ -0,0 +1,30 @@
+﻿// *******************************************
+// Simple
+// *******************************************
+
+using System.Collections.Generic;
+using Autodesk.AutoCAD.Runtime;
+using NUnitLite;
+using TestRunnerACAD;
+
+[assembly: CommandClass(typeof(TestRunner))]
+namespace TestRunnerACAD
+{
+    public static class TestRunner
+    {
+        [CommandMethod("RunTests", CommandFlags.Session)]
+        public static void RunTests()
+        {
+            string[] nunitArgs = new List<string>
+            {
+                // for details of options see  https://github.com/nunit/docs/wiki/Console-Command-Line
+                "--trace=verbose" // Tell me everything
+                ,"--result=" + "report.xml"
+                // , "--work=" + directoryName // save TestResults.xml to the build folder
+                //, "--wait" // Wait for input before closing console window (PAUSE). Comment this out for batch operations.
+            }.ToArray();
+
+            new AutoRun().Execute(nunitArgs);
+        }
+    }
+}
